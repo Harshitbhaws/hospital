abd931b165b0c6e24f603359b5e5c34da1fffa54
Harshitbhaws <habhawsar@bestpeers.com>
2021-10-01 22:28:01 +0530

    fourth commit

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,8 @@
 class Patient < ApplicationRecord
+
+    validates :name, presence: true
+    validates :phone, presence: true
+    validates :age, presence: true
+    validates :address, presence: true
+    validates :disease, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  
     root "doctors#index"
-    
+    devise_for :users
     resources :doctors
     resources :patients
 end
