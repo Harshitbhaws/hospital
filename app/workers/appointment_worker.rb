d5a53b8a0f1d69c05e47426022b6f3eecd05a301
Harshitbhaws <habhawsar@bestpeers.com>
2021-10-27 11:44:15 +0530

    add

--- app/workers/appointment_worker.rb
+++ app/workers/appointment_worker.rb
@@ -5,10 +5,21 @@ class AppointmentWorker
     # Do something
     appointment = Appointment.find(appointment_id)
     user = User.find(user_id)
+    html_string = AppointmentsController.renderer.render(
+  file: "appointments/show",
+  :formats => [:pdf,:html],
+  locals: {
+    :@routing_form => self,
+    :controller_name => appointments_controller,
+    :action_name => download_appointment,
+    :current_user => current_user
+  },
+  :layout  => '/layouts/application'
+)
   #   respond_to do |format|
   #     format.html
   #     format.pdf do
-  #         render pdf: "Invoice No. #{@appointment.id}",
+  #         render pdf: "Invoice No. #{appointment_id}",
   #         page_size: 'A4',
   #         template: "appointments/show.html.erb",
   #         layout: "pdf.html",
@@ -17,21 +28,21 @@ class AppointmentWorker
   #         zoom: 1,
   #         dpi: 75
   #     end
-  # end
-    view = html = ActionView::Base.new(Rails.root.join('app/views', 'false' ))
-    view.class.include ApplicationHelper
-    view.class.include Rails.application.routes.url_helpers
+  # # end
+  #   view = html = ActionView::Base.new(Rails.root.join('app/views'))
+  #   view.class.include ApplicationHelper
+  #   view.class.include Rails.application.routes.url_helpers
   
-    rendered = view.render(
-      :template => "appointments/show.html.erb",
-      :layout => "layouts/pdf.html.erb",
-      :locals => { :@appointment => appointment}
-    )
-    pdf = WickedPdf.new.pdf_from_string(rendered)
-    save_path = Rails.root.join('public', 'appointment.pdf')
-    File.open(save_path, 'wb') do |file|
-      file << pdf
-    end
-      save_path
+  #   rendered = view.render(
+  #     :template => "appointments/show.html.erb",
+  #     :layout => "layouts/pdf.html.erb",
+  #     :locals => { :@appointment => appointment}
+  #   )
+  #   pdf = WickedPdf.new.pdf_from_string(rendered)
+  #   save_path = Rails.root.join('public', 'appointment.pdf')
+  #   File.open(save_path, 'wb') do |file|
+  #     file << pdf
+  #   end
+  #     save_path
   end
 end
